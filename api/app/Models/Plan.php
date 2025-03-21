5f9811677ad07c2cb62b1e3f8f03888bafe2cee3
Denis <capelete.denis@hotmail.com>
2025-03-21 14:50:17 -0300

    initial commit

--- api/app/Models/Plan.php
+++ api/app/Models/Plan.php
@@ -6,4 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
+    protected $fillable = [
+        'description',
+        'numberOfClients',
+        'price',
+        'gigabytesStorage',
+    ]; 
 }
