ce11ae9137ffbfeabc8b5ec81eafceb32bafcc54
Denis <capelete.denis@hotmail.com>
2025-03-22 15:30:45 -0300

    feat: create cardPlan to display plan data

---
+++ front/src/components/cardPlan.tsx
@@ -0,0 +1,18 @@
+export function CardPlan() {
+            return (
+                        <div className="w-[360px] rounded-xl bg-white space-y-5 py-5 text-gray-700 shadow-md shadow-gray-300">
+                                    <div className="bg-orange-500 w-[310px] px-5 py-2 text-white">
+                                                <p className="font-bold text-xl">Até 10 vistorias</p>
+                                                <p className="font-semibold text-lg"> / clientes ativos</p>
+                                    </div>
+                                    <div className="px-5">
+                                                <p className="font-semibold text-xl">Preço:</p>
+                                                <p className="text-3xl font-bold">R$ 87,00 <span className="font-normal text-xl">/mês</span></p>
+                                    </div>
+                                    <div className="px-5 pb-5">
+                                                <p className="text-xl">Armazenamento:</p>
+                                                <p className="text-3xl font-bold">10 GB</p>
+                                    </div>
+                        </div>
+            )
+}
