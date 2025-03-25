f33f866b42ab216a230730f681e88f7652ae46d3
Denis <capelete.denis@hotmail.com>
2025-03-25 07:18:42 -0300

    feat: create migration and model Contract

---
+++ api/database/migrations/2025_03_25_090450_create_contracts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+                $table->decimal('price', 13, 2);
+                $table->boolean('active')->default(false);
+                $table->date('payment_date')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
