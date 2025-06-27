8c84be1ec0e09c8b17e19ae4c0db8f3260b760d5
Denis <capelete.denis@hotmail.com>
2025-06-27 20:20:39 -0300

    feat: initial config react vite

--- front/src/pages/Home/index.tsx
+++
@@ -1,9 +0,0 @@
-export const Home = () => {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-orange-400 text-2xl">
-        Desafio para Desenvolvedor - Inmediam
-      </h1>
-    </div>
-  )
-}
