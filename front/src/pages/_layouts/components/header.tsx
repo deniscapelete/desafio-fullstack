92f01b55663df4a48c709c61aaaf39eb59843ff6
Denis <capelete.denis@hotmail.com>
2025-06-28 08:50:05 -0300

    feat: add header to app layout

---
+++ front/src/pages/_layouts/components/header.tsx
@@ -0,0 +1,23 @@
+import logo from "@/assets/logo.png"
+import { FileText, Home } from "lucide-react"
+import { NavLink } from "./nav-link"
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <img src={logo} alt="logo inmediam" className="h-8" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            Início
+          </NavLink>
+          <NavLink to="/planos">
+            <FileText className="h-4 w-4" />
+            Planos
+          </NavLink>
+        </nav>
+      </div>
+    </div>
+  )
+}
