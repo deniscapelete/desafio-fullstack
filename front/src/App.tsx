66f82df35aa17fdca19e8b43123ba55edbc24d09
Denis <capelete.denis@hotmail.com>
2025-06-28 17:57:28 -0300

    feat: config API client with react-query

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,15 @@
 import { router } from "@/routes";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
 
   return (
     <div>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ div>
   )
 }
