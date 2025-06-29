fc8bf32af8fcfa0c8fb7bcd95ed66b23a66a849c
Denis <capelete.denis@hotmail.com>
2025-06-29 10:57:11 -0300

    feat:add contract and payment table

---
+++ api/app/Models/Contract.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contract extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasMany(User::class);
+    }
+}
---
+++ api/database/migrations/2025_06_29_130531_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contract_id')->constrained('contracts')->onDelete('cascade');
+            $table->date('due_date');
+            $table->decimal('amount_due ', 13, 2);
+            $table->string("method_payment");
+            $table->string("category_payment");
+            $table->date('date_payment')->nullable();
+            $table->decimal('amount_paid', 13, 2)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
