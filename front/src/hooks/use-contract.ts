a251c12807dad5ef1ee8a41e5ac579a1f4d88ab8
Denis <capelete.denis@hotmail.com>
2025-07-03 06:49:20 -0300

    refactor(use-contract): handle error status and update contract cache active

--- front/src/hooks/use-contract.ts
+++ front/src/hooks/use-contract.ts
@@ -1,22 +1,37 @@
 import { createContract } from "@/api/create-contract"
 import { useMutation } from "@tanstack/react-query"
 import { toast } from "sonner"
+import { AxiosError } from 'axios'
+import { queryClient } from "@/lib/react-query"
 
 export function useContract() {
 
-    const {
+  const {
     mutateAsync: createContractFn,
     isPending: isPendingCreateContract,
   } = useMutation({
     mutationFn: createContract,
-    onSuccess() {
+    onSuccess(data) {
       toast.success("Plano contratado com sucesso.")
+      queryClient.setQueryData(['contract-active'], data)
     },
-    onError() {
-      toast.error("Ocorreu um erro")
+    onError(error: AxiosError<any>) { /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    const response = error.response
+    const statusCode = response?.status || 500
+    const errorMessages: { [key: number]: ()=> string} =
+      {
+        404: () => response?.data.message,
+        422: () => {
+          const validationErrors = response?.data.errors
+          return Object.values(validationErrors).join(' ')
+        },
+        500: () => 'Ocorreu um erro.',
+      }
+     const message = errorMessages[statusCode];
+     toast.error(message)
     }
   })
-
+  
   return {
     createContractFn,
     isPendingCreateContract
